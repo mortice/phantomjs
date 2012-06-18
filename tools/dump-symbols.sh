3c5712db97e4540ef97b7a6dc7d6193cf89e436a
Tom Stuart <tom@therye.org>
2012-06-18 22:35:38 +0100

    Add breakpad support for Mac
    WiP for @jonleighton

--- tools/dump-symbols.sh
+++ tools/dump-symbols.sh
@@ -16,14 +16,19 @@ rm -r symbols/*
 
 files=""
 files+="bin/phantomjs "
-files+="src/qt/lib/libQtCore.so.4.8.0 "
-files+="src/qt/lib/libQtWebKit.so.4.9.0 "
-files+="src/qt/lib/libQtGui.so.4.8.0 "
-files+="src/qt/lib/libQtNetwork.so.4.8.0"
+if [[ $OSTYPE = linux* ]]; then
+  files+="src/qt/lib/libQtCore.so.4.8.0 "
+  files+="src/qt/lib/libQtWebKit.so.4.9.0 "
+  files+="src/qt/lib/libQtGui.so.4.8.0 "
+  files+="src/qt/lib/libQtNetwork.so.4.8.0"
+  dump_syms="src/breakpad/src/tools/linux/dump_syms/dump_syms"
+elif [[ $OSTYPE = darwin* ]]; then
+  dump_syms="tools/dump_syms.app/Contents/MacOS/dump_syms"
+fi
 
 for file in $files; do
     name=`basename $file`
-    src/breakpad/src/tools/linux/dump_syms/dump_syms $file > $name.sym
+    $dump_syms $file > $name.sym
     dir=symbols/$name/`head -n1 $name.sym | cut -d ' ' -f 4`
     mkdir -p $dir
     mv $name.sym $dir
